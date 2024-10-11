b94852a1eb974fb2fb5d09118ba2f6216d9dbd90
TheWolfStreet <wolfthestreet@gmail.com>
2024-10-11 06:00:49 +0300

    Remove search params in librewolf

--- home-manager/browser.nix
+++ home-manager/browser.nix
@@ -32,11 +32,6 @@ in {
         id = 0;
         name = "default";
         isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://searx.aicampground.com";
-          "browser.search.defaultenginename" = "Searx";
-          "browser.search.order.1" = "Searx";
-        };
       };
     };
     policies = {
