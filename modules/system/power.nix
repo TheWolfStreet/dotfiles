f2bf7ef2e499034c7e9f64c7e500db7f85bc66c2
TheWolfStreet <wolfthestreet@gmail.com>
2026-01-25 20:44:28 +0200

    force disable powersaving for ethernet port

--- modules/system/power.nix
+++ modules/system/power.nix
@@ -9,46 +9,47 @@
   };
 
   config = lib.mkIf config.power.enable {
-    powerManagement.powertop.enable = true;
+    environment.systemPackages = [pkgs.powertop];
 
-    boot.kernelModules = ["msr"];
+    boot = {
+      kernelModules = ["msr"];
+      kernelParams = [
+        "snd_hda_intel.power_save=1"
+        "nmi_watchdog=0"
+      ];
+    };
 
     powerManagement = {
       enable = true;
       cpuFreqGovernor = "schedutil";
     };
 
-    systemd.services = {
-      enable-power-actions = {
-        description = "Enable power profile actions";
-        after = ["power-profiles-daemon.service"];
-        wants = ["power-profiles-daemon.service"];
-        wantedBy = ["multi-user.target"];
+    systemd.services.enable-power-actions = {
+      description = "Enable power profile actions";
+      after = ["power-profiles-daemon.service"];
+      wants = ["power-profiles-daemon.service"];
+      wantedBy = ["multi-user.target"];
 
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-          ExecStart = pkgs.writeShellScript "enable-power-actions" ''
-            ${pkgs.power-profiles-daemon}/bin/powerprofilesctl configure-action --enable amdgpu_panel_power
-            ${pkgs.power-profiles-daemon}/bin/powerprofilesctl configure-action --enable amdgpu_dpm
-          '';
-        };
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = pkgs.writeShellScript "enable-power-actions" ''
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl configure-action --enable amdgpu_panel_power
+          ${pkgs.power-profiles-daemon}/bin/powerprofilesctl configure-action --enable amdgpu_dpm
+        '';
       };
     };
 
-    services = {
-      udev.extraRules = lib.mkMerge [
-        ''ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{class}!="0x02*", ATTR{power/control}="auto"''
-        ''ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"''
-
-        ''ACTION=="add", SUBSYSTEM=="pci", ATTR{class}=="0x0200*", ATTR{power/control}="on"''
-
-        ''ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", ATTR{link_power_management_policy}="med_power_with_dipm"''
-
-        ''ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{bDeviceClass}!="03", ATTR{bInterfaceClass}!="01", ATTR{bInterfaceClass}!="03", ATTR{power/control}="auto"''
-
-        ''ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{power/autosuspend_delay_ms}="60000"''
-      ];
-    };
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{class}=="0x02*", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{class}=="0x0d11*", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="pci", TEST=="power/control", ATTR{class}!="0x02*", ATTR{class}!="0x0d11*", ATTR{power/control}="auto"
+      ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"
+      ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", ATTR{link_power_management_policy}="med_power_with_dipm"
+      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{bInterfaceClass}=="e0", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{bDeviceClass}=="e0", ATTR{power/control}="on"
+      ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{bDeviceClass}!="03", ATTR{bInterfaceClass}!="01", ATTR{bInterfaceClass}!="03", ATTR{bInterfaceClass}!="e0", ATTR{power/control}="auto"
+      ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{power/autosuspend_delay_ms}="60000"
+    '';
   };
 }
