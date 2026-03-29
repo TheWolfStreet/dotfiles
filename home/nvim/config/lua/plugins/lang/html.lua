f61dee93332ba212a4ad4d42786797fee2b26739
TheWolfStreet <wolfthestreet@gmail.com>
2026-03-30 00:47:32 +0300

    Sync

--- home/nvim/config/lua/plugins/lang/html.lua
+++ home/nvim/config/lua/plugins/lang/html.lua
@@ -1,3 +1,23 @@
 return {
-    { import = "lazyvim.plugins.extras.lang.html" },
+    -- LazyVim doesn't ship an `extras.lang.html` module.
+    -- Keep HTML/CSS support here explicitly.
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "html",
+                "css",
+                "scss",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+            opts.servers = opts.servers or {}
+            -- These servers are typically provided by `vscode-langservers-extracted`.
+            opts.servers.html = opts.servers.html or {}
+            opts.servers.cssls = opts.servers.cssls or {}
+        end,
+    },
 }
